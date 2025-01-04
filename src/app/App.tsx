286f03e9b733b6c9c413c4b7f3f1a3a4b59f15d0
Vasyl V <71288130+fantomblackstar@users.noreply.github.com>
2025-01-04 17:48:53 +0200

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./global.css";
+
+function App() {
+  return (
+    <div>
+      <p className="text-red-500">fdfd</p>
+    </div>
+  );
+}
+
+export default App;
